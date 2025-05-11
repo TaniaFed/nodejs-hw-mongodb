20b701337449dcec6e5197c769fa1f0dde0947d5
TaniaFed <ktv.poshta@gmail.com>
2025-05-10 20:04:26 -0400

    hw-2 bugs fixed

--- src/server.js
+++ src/server.js
@@ -30,6 +30,36 @@ export const setupServer = () => {
         });
     });
 
+    app.get('/contacts', async (req, res) => {
+
+        const contacts = await getAllContacts();
+
+        res.status(200).json({
+            status: 200,
+            message: 'Successfully found contacts!',
+            data: contacts,
+        });
+    });
+
+    app.get('/contacts/:contactId', async (req, res) => {
+        const { contactId } = req.params;
+
+        const contact = await getContactById(contactId);
+
+        if (!contact) {
+            return (res.status(404).json({
+                message: 'Contact not found',
+            }));
+
+        };
+
+        res.status(200).json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}!`,
+            data: { contact },
+        });
+    });
+
     app.use((req, res, next) => {
         res.status(404).json({
             message: 'Not found',
@@ -50,40 +80,6 @@ export const setupServer = () => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
-
-    app.get('/contacts', async (req, res) => {
-
-        const contacts = await getAllContacts();
-
-        res.status(200).json({
-            message: 'Successfully found contacts!',
-            data: contacts,
-        });
-    });
-
-    app.get('/contacts/:contactId', async (req, res, next) => {
-        const { contactId } = req.params;
-
-        const contact = await getContactById(contactId);
-
-        if (!contact) {
-            res.status(404).json({
-                message: 'Contact not found',
-            });
-
-            return;
-        }
-
-        res.status(200).json({
-            message: `Successfully found contact with id ${contactId}!`,
-            data: { contact },
-        });
-
-        next();
-
-    });
-
-
 };
 
 
